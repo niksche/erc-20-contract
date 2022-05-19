a7f1d8ec195e5bc24efe37c9cef47b76d7ff6934
Nikita Shchelkanov <nik.schelkanov@gmail.com>
2022-05-19 15:41:19 +0300

    added config for rinkeby and write first test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,17 +7,10 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
+import "./tasks/task";
+
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -29,6 +22,11 @@ const config: HardhatUserConfig = {
       url: process.env.ROPSTEN_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.RB_PRIVATE_KEY !== undefined ? [process.env.RB_PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,37 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
+import { ERC20 } from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("CryptonToken", function () {
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  let CryptonTokenContract: ERC20;
+  let owner: SignerWithAddress;
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  beforeEach(async function () {
+    const erc20Factory = await ethers.getContractFactory("ERC20");
+    CryptonTokenContract = await erc20Factory.deploy("Crypton coin", "CRP",1);
+    console.log(CryptonTokenContract.address);
+    [owner] = await ethers.getSigners();
+  })
+  
+  describe("Deployment", function() {
+    it("Should deploy our contract", async function() {
+      const totalSupply = await CryptonTokenContract.totalSupply();
+      expect(totalSupply ===  BigNumber.from("0"), "Initial supply should be equal to zerro");
+    })
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    it("Signer's balance should be zerro", async function() {
+      const ownersBalance = await CryptonTokenContract.balanceOf(owner.address);
+      console.log(ownersBalance);
+      expect(ownersBalance === BigNumber.from("0"), "owner balance should be equal to zerro");
+    })
+  })
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+  
+
+
+
 });
+
